a5ce3f1f38337dc392a6897cd6dd2dc67cf59ad3
havanduoc <havanduoc.dev@gmail.com>
2024-10-03 23:39:22 +0700

    Fix - Grid layout for Choose Us Section

--- src/components/home/whyChooseUsSection/ChooseUsSection.tsx
+++ src/components/home/whyChooseUsSection/ChooseUsSection.tsx
@@ -3,7 +3,7 @@ import BoxLayoutSection from "@/components/common/BoxLayoutSection";
 import TitleComponent from "@/components/common/TitleComponent";
 import React from "react";
 import Image from "next/image";
-import { CHOOSES } from "@/data/home/choooseUs";
+import { CHOOSES } from "@/data/home/chooseUs";
 
 const ChooseUsSection = () => {
   return (
@@ -13,22 +13,20 @@ const ChooseUsSection = () => {
           title="Why choose us?"
           gradientWords={["choose", "us?"]}
         />
-        <div className="flex flex-wrap gap-5 sm:gap-8 items-center justify-center">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5 sm:gap-8 items-center justify-center">
           {CHOOSES.map((choose, index) => (
             <div
-              className={`flex-1 min-w-[200px] max-w-[290px] ${
-                index % 2 === 0 ? "md:pb-10" : "md:pt-10"
-              }`}
+              className={` ${index % 2 === 0 ? "md:pb-10" : "md:pt-10"}`}
               key={choose.id}
             >
               <div className="space-y-5 sm:space-y-7 border border-gray-300 py-10 px-8 rounded-lg">
-                  <Image
-                    src={choose.icon}
-                    alt=""
-                    className="w-[35px] sm:w-[40px] h-auto object-cover"
-                  />
-                  <h4>{choose.name}</h4>
-                  <p>{choose.content}</p>
+                <Image
+                  src={choose.icon}
+                  alt=""
+                  className="w-[35px] sm:w-[40px] h-auto object-cover"
+                />
+                <h4>{choose.name}</h4>
+                <p>{choose.content}</p>
               </div>
             </div>
           ))}
